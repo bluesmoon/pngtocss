d426483cc73d00c248bb3ba1576e37f82e818b5e
Philip Tellis <philip.tellis@gmail.com>
2011-04-23 01:52:38 -0700

    added URL to version info

--- src/pngtocss.c
+++ src/pngtocss.c
@@ -72,6 +72,9 @@ static void print_error(const char *fname, status err)
 static void version_info()
 {
 	fprintf(stderr, "pngtocss v%s\n", VERSION);
+	fprintf(stderr, "   Copyright 2011 Philip Tellis\n");
+	fprintf(stderr, "   https://github.com/bluesmoon/pngtocss\n\n");
+	fprintf(stderr, "   Distributed under the terms of the BSD license\n\n");
 	fprintf(stderr, "   Compiled with libpng %s; using libpng %s.\n",
 			PNG_LIBPNG_VER_STRING, png_libpng_ver);
 	fprintf(stderr, "   Compiled with zlib %s; using zlib %s.\n\n",
